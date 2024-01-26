0e92584fb41595505ab20cd6efe694f295c07b47
Bryant-Huang-BU <115976062+Bryant-Huang-BU@users.noreply.github.com>
2024-01-25 23:30:55 -0600

    modified output

--- MessageOutput.java
+++ MessageOutput.java
@@ -18,4 +18,23 @@ public class MessageOutput extends Object{
     public getOut() {
         return out;
     }
+
+    public void writeBytes(byte[] bytes, int off, int len) throws IOException {
+        if (bytes == null) {
+            throw new NullPointerException("bytes is null");
+        }
+        if (off < 0) {
+            throw new IndexOutOfBoundsException("off is less than 0");
+        }
+        if (len < 0) {
+            throw new IndexOutOfBoundsException("len is less than 0");
+        }
+        if (len > bytes.length - off) {
+            throw new IndexOutOfBoundsException("len is greater than bytes.length - off");
+        }
+        if (len == 0) {
+            return;
+        }
+        out.write(bytes, off, len);
+    }
 }
