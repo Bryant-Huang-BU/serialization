4ca0e65e078ddf3b8093db6e42c086b44e02b158
Bryant-Huang-BU <Bryant_Huang1@Baylor.edu>
2024-03-27 20:18:54 -0500

    TO DO: COMMENTING AND FIXING DOWNLOAD SERVICE

---
+++ klab/app/DownloadSender.java
@@ -0,0 +1,2 @@
+package klab.app;public class DownloadSender {
+}
---
+++ klab/app/UltimateManagement.java
@@ -0,0 +1,24 @@
+package klab.app;
+
+import java.net.Socket;
+
+public class UltimateManagement implements Runnable {
+    private Socket socket;
+    private ResponseManagement rM;
+    public UltimateNode(Socket socket) {
+        this.socket = socket;
+        this.rM = new ResponseManagement(socket);
+    }
+    @Override
+    public void run() {
+        Node.LOGGER.info("Ultimate Node thread running for socket: " 
+        + socket.getInetAddress() + ":" + socket.getPort());
+        try {
+            Thread rMT = new Thread(rM);
+            rMT.start();
+        } catch (Exception e) {
+            Node.LOGGER.warning("Error in Ultimate Node thread");
+        }
+    }
+    
+}
