4414b66d8cd3080328385a4d0acd82b689db59f1
Bryant-Huang-BU <115976062+Bryant-Huang-BU@users.noreply.github.com>
2024-03-30 20:36:08 -0500

    help me

---
+++ metanode/MessageType.java
@@ -0,0 +1,10 @@
+public enum MessageType extends Enum<MessageType> {
+    public static final MessageType AnswerRequest;
+    public static final MessageType RequestNodes;
+
+    public static final MessageType NodeAdditions;
+    public static final MessageType MetaNodeAdditions;
+    public static final MessageType NodeDeletions;
+    public static final MessageType MetaNodeDeletions;
+    public static final MessageType ClearCache;
+}
